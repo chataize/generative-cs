598794edbe8e4690d874016c9d9d8aee86d355ae
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-12-17 15:22:08 +0100

    Add ICompletionProvider interface

---
+++ src/Interfaces/ICompletionProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace GenerativeCS.Interfaces;
+
+public interface ICompletionProvider
+{
+    Task<string> CompleteAsync(string prompt);
+
+    Task<string> CompleteAsync(IChatConversation conversation);
+}
