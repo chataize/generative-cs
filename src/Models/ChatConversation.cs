24d530586b4bd8a620b5cc5f35fd0f3a21b8c1f8
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-12-17 16:04:57 +0100

    Add ChatConversation model record

---
+++ src/Models/ChatConversation.cs
@@ -0,0 +1,47 @@
+﻿using GenerativeCS.Interfaces;
+
+namespace GenerativeCS.Models;
+
+public record ChatConversation : IChatConversation<List<ChatMessage>, ChatMessage>
+{
+    public ChatConversation() { }
+
+    public ChatConversation(string systemMessage)
+    {
+        Messages.Add(ChatMessage.FromSystem(systemMessage));
+    }
+
+    public List<ChatMessage> Messages { get; set; } = [];
+
+    public DateTimeOffset CreationTime { get; set; } = DateTimeOffset.Now;
+
+    public void FromSystem(string message)
+    {
+        Messages.Add(ChatMessage.FromSystem(message));
+    }
+
+    public void FromUser(string message)
+    {
+        Messages.Add(ChatMessage.FromUser(message));
+    }
+
+    public void FromUser(string name, string message)
+    {
+        Messages.Add(ChatMessage.FromUser(name, message));
+    }
+
+    public void FromAssistant(string message)
+    {
+        Messages.Add(ChatMessage.FromAssistant(message));
+    }
+
+    public void FromAssistant(IFunctionCall functionCall)
+    {
+        Messages.Add(ChatMessage.FromAssistant(functionCall));
+    }
+
+    public void FromFunction(string name, string message)
+    {
+        Messages.Add(ChatMessage.FromFunction(name, message));
+    }
+}
