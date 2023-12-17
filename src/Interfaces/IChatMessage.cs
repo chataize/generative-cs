1dd099d74881171ee728879963425a06e715ef08
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-12-17 15:17:53 +0100

    Add IChatMessage interface

---
+++ src/Interfaces/IChatMessage.cs
@@ -0,0 +1,14 @@
+﻿using GenerativeCS.Enums;
+
+namespace GenerativeCS.Interfaces;
+
+public interface IChatMessage
+{
+    ChatRole Role { get; set; }
+
+    string? Name { get; set; }
+
+    string Content { get; set; }
+
+    IFunctionCall? FunctionCall { get; set; }
+}
