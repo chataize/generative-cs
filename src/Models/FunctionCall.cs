4577240eae317d5cc3954be6e27c93fc56c7494f
Marcel Kwiatkowski <kwiatkowski.marcel@protonmail.com>
2023-12-17 16:07:10 +0100

    Add FunctionCall model record

---
+++ src/Models/FunctionCall.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using GenerativeCS.Interfaces;
+
+namespace GenerativeCS.Models;
+
+public record FunctionCall : IFunctionCall
+{
+    public FunctionCall() { }
+
+    [SetsRequiredMembers]
+    public FunctionCall(string name, string arguments)
+    {
+        Name = name;
+        Arguments = arguments;
+    }
+
+    public required string Name { get; set; }
+
+    public required string Arguments { get; set; }
+}
