e5b0b444c679c87320b39cc98a0949215073a518
Marcel Kwiatkowski <marcel2215@icloud.com>
2023-12-17 21:15:37 +0100

    Add helper methods to initialize message

--- src/Interfaces/IChatMessage.cs
+++ src/Interfaces/IChatMessage.cs
@@ -11,4 +11,59 @@ public interface IChatMessage
     string? Content { get; set; }
 
     IFunctionCall? FunctionCall { get; set; }
+
+    static T FromSystem<T>(string content) where T : IChatMessage, new()
+    {
+        return new T
+        {
+            Role = ChatRole.System,
+            Content = content
+        };
+    }
+
+    static T FromUser<T>(string content) where T : IChatMessage, new()
+    {
+        return new T
+        {
+            Role = ChatRole.User,
+            Content = content
+        };
+    }
+
+    static T FromUser<T>(string name, string content) where T : IChatMessage, new()
+    {
+        return new T
+        {
+            Role = ChatRole.User,
+            Name = name,
+            Content = content
+        };
+    }
+
+    static T FromAssistant<T>(string content) where T : IChatMessage, new()
+    {
+        return new T
+        {
+            Role = ChatRole.Assistant,
+            Content = content
+        };
+    }
+
+    static T FromAssistant<T>(IFunctionCall functionCall) where T : IChatMessage, new()
+    {
+        return new T
+        {
+            FunctionCall = functionCall
+        };
+    }
+
+    static T FromFunction<T>(string name, string content) where T : IChatMessage, new()
+    {
+        return new T
+        {
+            Role = ChatRole.Function,
+            Name = name,
+            Content = content
+        };
+    }
 }
