4c41b70cb57557cf09e8f12d30c2917e87f542f4
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-01-14 22:02:34 +0100

    Make OpenAIClientOptions record properties non-nullable

--- src/Options/OpenAI/OpenAIClientOptions.cs
+++ src/Options/OpenAI/OpenAIClientOptions.cs
@@ -4,15 +4,15 @@ public record OpenAIClientOptions
 {
     public required string ApiKey { get; set; }
 
-    public ChatCompletionOptions? DefaultCompletionOptions { get; set; } = new();
+    public ChatCompletionOptions DefaultCompletionOptions { get; set; } = new();
 
-    public EmbeddingOptions? DefaultEmbeddingOptions { get; set; } = new();
+    public EmbeddingOptions DefaultEmbeddingOptions { get; set; } = new();
 
-    public TextToSpeechOptions? DefaultTextToSpeechOptions { get; set; } = new();
+    public TextToSpeechOptions DefaultTextToSpeechOptions { get; set; } = new();
 
-    public TranscriptionOptions? DefaultTranscriptionOptions { get; set; } = new();
+    public TranscriptionOptions DefaultTranscriptionOptions { get; set; } = new();
 
-    public TranslationOptions? DefaultTranslationOptions { get; set; } = new();
+    public TranslationOptions DefaultTranslationOptions { get; set; } = new();
 
-    public ModerationOptions? DefaultModerationOptions { get; set; } = new();
+    public ModerationOptions DefaultModerationOptions { get; set; } = new();
 }
