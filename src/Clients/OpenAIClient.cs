8ce518d47497d7b98d4be17b3932d963f812d660
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-01-19 15:25:52 +0100

    Add methods to quickly manage client chat functions

--- src/Clients/OpenAIClient.cs
+++ src/Clients/OpenAIClient.cs
@@ -207,4 +207,116 @@ public class OpenAIClient
     {
         return await Moderation.ModerateAsync(text, ApiKey, options ?? DefaultModerationOptions, _httpClient, cancellationToken);
     }
+
+    public void AddFunction(ChatFunction function)
+    {
+        DefaultCompletionOptions.Functions.Add(function);
+    }
+
+    public void AddFunction(string name, bool requiresConfirmation = false)
+    {
+        DefaultCompletionOptions.Functions.Add(new ChatFunction(name, requiresConfirmation));
+    }
+
+    public void AddFunction(string name, string? description, bool requiresConfirmation = false)
+    {
+        DefaultCompletionOptions.Functions.Add(new ChatFunction(name, description, requiresConfirmation));
+    }
+
+    public void AddFunction(Delegate callback)
+    {
+        DefaultCompletionOptions.Functions.Add(new ChatFunction(callback));
+    }
+
+    public void AddFunction(string name, Delegate callback)
+    {
+        DefaultCompletionOptions.Functions.Add(new ChatFunction(name, callback));
+    }
+
+    public void AddFunction(string name, IEnumerable<FunctionParameter> parameters)
+    {
+        DefaultCompletionOptions.Functions.Add(new ChatFunction(name, parameters));
+    }
+
+    public void AddFunction(string name, params FunctionParameter[] parameters)
+    {
+        DefaultCompletionOptions.Functions.Add(new ChatFunction(name, parameters));
+    }
+
+    public void AddFunction(string name, string? description, Delegate callback)
+    {
+        DefaultCompletionOptions.Functions.Add(new ChatFunction(name, description, callback));
+    }
+
+    public void AddFunction(string name, string? description, IEnumerable<FunctionParameter> parameters)
+    {
+        DefaultCompletionOptions.Functions.Add(new ChatFunction(name, description, parameters));
+    }
+
+    public void AddFunction(string name, string? description, params FunctionParameter[] parameters)
+    {
+        DefaultCompletionOptions.Functions.Add(new ChatFunction(name, description, parameters));
+    }
+
+    public void AddFunction(string name, bool requiresConfirmation, Delegate callback)
+    {
+        DefaultCompletionOptions.Functions.Add(new ChatFunction(name, requiresConfirmation, callback));
+    }
+
+    public void AddFunction(string name, bool requiresConfirmation, IEnumerable<FunctionParameter> parameters)
+    {
+        DefaultCompletionOptions.Functions.Add(new ChatFunction(name, requiresConfirmation, parameters));
+    }
+
+    public void AddFunction(string name, bool requiresConfirmation, params FunctionParameter[] parameters)
+    {
+        DefaultCompletionOptions.Functions.Add(new ChatFunction(name, requiresConfirmation, parameters));
+    }
+
+    public void AddFunction(string name, string? description, bool requiresConfirmation, Delegate callback)
+    {
+        DefaultCompletionOptions.Functions.Add(new ChatFunction(name, description, requiresConfirmation, callback));
+    }
+
+    public void AddFunction(string name, string? description, bool requiresConfirmation, IEnumerable<FunctionParameter> parameters)
+    {
+        DefaultCompletionOptions.Functions.Add(new ChatFunction(name, description, requiresConfirmation, parameters));
+    }
+
+    public void AddFunction(string name, string? description, bool requiresConfirmation, params FunctionParameter[] parameters)
+    {
+        DefaultCompletionOptions.Functions.Add(new ChatFunction(name, description, requiresConfirmation, parameters));
+    }
+
+    public bool RemoveFunction(ChatFunction function)
+    {
+        return DefaultCompletionOptions.Functions.Remove(function);
+    }
+
+    public bool RemoveFunction(string name)
+    {
+        var function = DefaultCompletionOptions.Functions.LastOrDefault(f => f.Name == name);
+        if (function == null)
+        {
+            return false;
+        }
+
+        return DefaultCompletionOptions.Functions.Remove(function);
+    }
+
+    public bool RemoveFunction(Delegate callback)
+    {
+        var function = DefaultCompletionOptions.Functions.LastOrDefault(f => f.Callback == callback);
+        if (function == null)
+        {
+            return false;
+        }
+
+        return DefaultCompletionOptions.Functions.Remove(function);
+    }
+
+    public void ClearFunctions()
+    {
+        DefaultCompletionOptions.Functions.Clear();
+    }
 }
