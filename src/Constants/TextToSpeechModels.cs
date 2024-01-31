4f2a88d5a309d706bf40e3a959153d7f858a5a1f
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-01-31 19:02:10 +0100

    Add space between lines

--- src/Constants/TextToSpeechModels.cs
+++ src/Constants/TextToSpeechModels.cs
@@ -3,5 +3,6 @@ namespace ChatAIze.GenerativeCS.Constants;
 public static class TextToSpeechModels
 {
     public const string TTS_1 = "tts-1";
+    
     public const string TTS_1_HD = "tts-1-hd";
 }
