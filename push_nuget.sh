95de19d2585febc5818a7c26277fba287a867f02
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-01-31 21:51:09 +0100

    Update push_nuget.sh script to publish the latest package version

--- push_nuget.sh
+++ push_nuget.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-dotnet nuget push ./src/bin/Release/ChatAIze.GenerativeCS.0.2.0.nupkg --api-key $CHATAIZE_NUGET_API_KEY --source https://api.nuget.org/v3/index.json
+
+dotnet build
+dotnet publish
+dotnet pack
+
+latest_package=$(ls ./src/bin/Release/ChatAIze.GenerativeCS.*.*.*.nupkg | sort -V | tail -n 1)
+dotnet nuget push "$latest_package" --api-key $CHATAIZE_NUGET_API_KEY --source https://api.nuget.org/v3/index.json
