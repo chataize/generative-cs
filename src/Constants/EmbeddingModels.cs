320cfdc48291e8ca979ec4255322a761d0e009f3
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-01-31 18:29:37 +0100

    Add constants for new embedding models

--- src/Constants/EmbeddingModels.cs
+++ src/Constants/EmbeddingModels.cs
@@ -3,4 +3,8 @@ namespace ChatAIze.GenerativeCS.Constants;
 public static class EmbeddingModels
 {
     public const string TEXT_EMBEDDING_ADA_002 = "text-embedding-ada-002";
+
+    public const string TEXT_EMBEDDING_3_SMALL = "text-embedding-3-small";
+
+    public const string TEXT_EMBEDDING_3_LARGE = "text-embedding-3-large";
 }
