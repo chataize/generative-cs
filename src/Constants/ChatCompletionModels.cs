8e1bc5aa7c1bb2104efe8c184b05bc21217a903d
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-01-31 18:35:31 +0100

    Add constants for new GPT models

--- src/Constants/ChatCompletionModels.cs
+++ src/Constants/ChatCompletionModels.cs
@@ -2,7 +2,13 @@ namespace ChatAIze.GenerativeCS.Constants;
 
 public static class ChatCompletionModels
 {
+    public const string GPT_3_5_TURBO_0125 = "gpt-3.5-turbo-0125";
+
     public const string GPT_3_5_TURBO_1106 = "gpt-3.5-turbo-1106";
-    public const string GPT_4_1106 = "gpt-4-1106-preview";
+
+    public const string GPT_4_0125_PREVIEW = "gpt-4-0125-preview";
+
+    public const string GPT_4_1106_PREVIEW = "gpt-4-1106-preview";
+
     public const string GEMINI_PRO = "gemini-pro";
 }
