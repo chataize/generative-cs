a9870cd6a09b82e717befd135180d7950347868b
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-03-26 18:14:36 +0100

    Fix wrong release folder path in push_nuget.sh script

--- tools/push_nuget.sh
+++ tools/push_nuget.sh
@@ -6,5 +6,5 @@ dotnet build
 dotnet publish
 dotnet pack
 
-latest_package=$(ls ./src/bin/Release/ChatAIze.GenerativeCS.*.*.*.nupkg | sort -V | tail -n 1)
+latest_package=$(ls ./bin/Release/ChatAIze.GenerativeCS.*.*.*.nupkg | sort -V | tail -n 1)
 dotnet nuget push "$latest_package" --api-key $CHATAIZE_NUGET_API_KEY --source https://api.nuget.org/v3/index.json
