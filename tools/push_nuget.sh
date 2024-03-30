ceb9f2f9b8c227fc86b308efa040709137aec599
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-03-30 18:18:27 +0100

    Update script to also push NuGet to GitHub Packages

--- tools/push_nuget.sh
+++ tools/push_nuget.sh
@@ -7,4 +7,6 @@ dotnet publish
 dotnet pack
 
 latest_package=$(ls ./bin/Release/ChatAIze.GenerativeCS.*.*.*.nupkg | sort -V | tail -n 1)
-dotnet nuget push "$latest_package" --api-key $CHATAIZE_NUGET_API_KEY --source https://api.nuget.org/v3/index.json
+
+dotnet nuget push "$latest_package" --api-key "$CHATAIZE_NUGET_API_KEY" --source "https://api.nuget.org/v3/index.json"
+dotnet nuget push "$latest_package" --api-key "$GITHUB_PAT" --source "https://nuget.pkg.github.com/chataize/index.json"
