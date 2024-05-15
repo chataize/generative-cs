7256c5fe25b07a35ec85455a5c7f440410fff85d
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-05-15 18:44:39 +0200

    Update and reorder OpenAI chat completion model name constants

--- src/Constants/ChatCompletionModels.cs
+++ src/Constants/ChatCompletionModels.cs
@@ -4,21 +4,23 @@ public static class ChatCompletionModels
 {
     public static class OpenAI
     {
-        public const string GPT35Turbo = "gpt-3.5-turbo";
+        public const string GPT4o = "gpt-4o";
 
-        public const string GPT35Turbo0301 = "gpt-3.5-turbo-0301";
+        public const string GPT4o20240513 = "gpt-4o-2024-05-13";
 
-        public const string GPT35Turbo0613 = "gpt-3.5-turbo-0613";
+        public const string GPT4Turbo = "gpt-4-turbo";
 
-        public const string GPT35Turbo0125 = "gpt-3.5-turbo-0125";
+        public const string GPT4Turbo20240409 = "gpt-4-turbo-2024-04-09";
+        
+        public const string GPT4TurboPreview = "gpt-4-turbo-preview";
 
-        public const string GPT35Turbo1106 = "gpt-3.5-turbo-1106";
+        public const string GPT40125Preview = "gpt-4-0125-preview";
 
-        public const string GPT35Turbo16k = "gpt-3.5-turbo-16k";
+        public const string GPT41106Preview = "gpt-4-1106-preview";
 
-        public const string GPT35Turbo16k0613 = "gpt-3.5-turbo-16k-0613";
+        public const string GPT4VisionPreview = "gpt-4-vision-preview";
 
-        public const string GPT35TurboInstruct = "gpt-3.5-turbo-instruct";
+        public const string GPT41106VisionPreview = "gpt-4-1106-vision-preview";
 
         public const string GPT4 = "gpt-4";
 
@@ -28,15 +30,23 @@ public static class ChatCompletionModels
 
         public const string GPT432k0613 = "gpt-4-32k-0613";
 
-        public const string GPT40125Preview = "gpt-4-0125-preview";
+        public const string GPT35Turbo0125 = "gpt-3.5-turbo-0125";
 
-        public const string GPT41106Preview = "gpt-4-1106-preview";
+        public const string GPT35Turbo = "gpt-3.5-turbo";
 
-        public const string GPT4Turbo = "gpt-4-turbo";
+        public const string GPT35Turbo1106 = "gpt-3.5-turbo-1106";
 
-        public const string GPT4TurboPreview = "gpt-4-turbo-preview";
+        public const string GPT35TurboInstruct = "gpt-3.5-turbo-instruct";
 
-        public const string GPT4o = "gpt-4o";
+        public const string GPT35Turbo16k = "gpt-3.5-turbo-16k";
+
+        public const string GPT35Turbo0613 = "gpt-3.5-turbo-0613";
+
+        public const string GPT35Turbo16k0613 = "gpt-3.5-turbo-16k-0613";
+
+        public const string Babbage002 = "babbage-002";
+
+        public const string Davinci002 = "davinci-002";
     }
 
     public static class Gemini
