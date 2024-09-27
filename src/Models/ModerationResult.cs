363d16b6d9cdad4c0aa2eff291c2cabacee30ffd
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-09-27 17:54:55 +0200

    Reorder ModerationResult record properties

--- src/Models/ModerationResult.cs
+++ src/Models/ModerationResult.cs
@@ -6,45 +6,45 @@ public record ModerationResult
 
     public bool IsSexual { get; set; }
 
-    public bool IsSexualMinors { get; set; }
-
-    public bool IsViolence { get; set; }
-
-    public bool IsViolenceGraphic { get; set; }
-
-    public bool IsHate { get; set; }
-
-    public bool IsHateThreatening { get; set; }
-
-    public bool IsHarassmentThreatening { get; set; }
-
-    public bool IsHarassment { get; set; }
-
-    public bool IsSelfHarm { get; set; }
-
-    public bool IsSelfHarmIntent { get; set; }
-
-    public bool IsSelfHarmInstruction { get; set; }
-
     public double SexualScore { get; set; }
+
+    public bool IsSexualMinors { get; set; }
 
     public double SexualMinorsScore { get; set; }
 
-    public double ViolenceScore { get; set; }
-
-    public double ViolenceGraphicScore { get; set; }
-
-    public double HateScore { get; set; }
-
-    public double HateThreateningScore { get; set; }
-
-    public double HarassmentThreateningScore { get; set; }
+    public bool IsHarassment { get; set; }
 
     public double HarassmentScore { get; set; }
 
+    public bool IsHarassmentThreatening { get; set; }
+
+    public double HarassmentThreateningScore { get; set; }
+
+    public bool IsHate { get; set; }
+
+    public double HateScore { get; set; }
+
+    public bool IsHateThreatening { get; set; }
+
+    public double HateThreateningScore { get; set; }
+
+    public bool IsSelfHarm { get; set; }
+
     public double SelfHarmScore { get; set; }
+
+    public bool IsSelfHarmIntent { get; set; }
 
     public double SelfHarmIntentScore { get; set; }
 
+    public bool IsSelfHarmInstruction { get; set; }
+
     public double SelfHarmInstructionScore { get; set; }
+
+    public bool IsViolence { get; set; }
+
+    public double ViolenceScore { get; set; }
+
+    public bool IsViolenceGraphic { get; set; }
+
+    public double ViolenceGraphicScore { get; set; }
 }
