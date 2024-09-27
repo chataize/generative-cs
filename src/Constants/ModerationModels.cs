fc9cc2f2cc682a5ba0bd57580fd130b14d7e3a23
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-09-27 17:50:48 +0200

    Add constants for new moderation models

--- src/Constants/ModerationModels.cs
+++ src/Constants/ModerationModels.cs
@@ -4,9 +4,13 @@ public static class ModerationModels
 {
     public static class OpenAI
     {
-        public const string TextModerationStable = "text-moderation-stable";
+        public const string OmniModerationLatest = "omni-moderation-latest";
+
+        public const string OmniModeration20240926 = "omni-moderation-2024-09-26";
 
         public const string TextModerationLatest = "moderation-latest";
+
+        public const string TextModerationStable = "text-moderation-stable";
 
         public const string TextModeration007 = "text-moderation-007";
     }
