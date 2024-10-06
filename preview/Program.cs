f1f4f4627243e82f0168e398700f7f699277524f
Marcel Kwiatkowski <marcel2215@icloud.com>
2024-10-06 02:42:48 +0200

    Add preview project

---
+++ preview/Program.cs
@@ -0,0 +1,28 @@
+﻿using ChatAIze.GenerativeCS.Clients;
+using ChatAIze.GenerativeCS.Models;
+using ChatAIze.GenerativeCS.Options.OpenAI;
+
+var client = new OpenAIClient();
+var conversation = new ChatConversation();
+
+var options = new ChatCompletionOptions
+{
+    IsIgnoringPreviousFunctionCalls = true,
+    IsDebugMode = true
+};
+
+options.AddFunction("Foo", (int number) => Console.WriteLine($"Bar [{number}]"));
+
+while (true)
+{
+    var input = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(input))
+    {
+        break;
+    }
+
+    conversation.FromUser(input);
+
+    var response = await client.CompleteAsync(conversation, options);
+    Console.WriteLine(response);
+}
