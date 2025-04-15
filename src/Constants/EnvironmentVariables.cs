78b143a144d254d1fbbddd83905dcd6d48c6ca96
Marcel Kwiatkowski <marcel2215@icloud.com>
2025-04-15 16:48:59 +0200

    Add space between lines

--- src/Constants/EnvironmentVariables.cs
+++ src/Constants/EnvironmentVariables.cs
@@ -3,5 +3,6 @@ namespace ChatAIze.GenerativeCS.Constants;
 internal static class EnvironmentVariables
 {
     internal const string OpenAIAPIKey = "OPENAI_API_KEY";
+    
     internal const string GeminiAPIKey = "GEMINI_API_KEY";
 }
