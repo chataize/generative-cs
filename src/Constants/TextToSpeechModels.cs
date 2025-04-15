a2141e62ee5785b271895abc5c7f6f9cd2d4ee7f
Marcel Kwiatkowski <marcel2215@icloud.com>
2025-04-15 16:31:12 +0200

    Add constant for new text-to-speech model

--- src/Constants/TextToSpeechModels.cs
+++ src/Constants/TextToSpeechModels.cs
@@ -7,5 +7,7 @@ public static class TextToSpeechModels
         public const string TTS1 = "tts-1";
 
         public const string TTS1HD = "tts-1-hd";
+
+        public const string GPT4oMiniTTS = "gpt-4o-mini-tts";
     }
 }
