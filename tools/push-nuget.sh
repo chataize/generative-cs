5509f9d29fe87cb435af4a59bd516276068ccc9a
Marcel Kwiatkowski <marcel2215@icloud.com>
2025-04-22 10:31:55 +0200

    Update push-nuget.sh script

--- tools/push-nuget.sh
+++ tools/push-nuget.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
 cd ../src
-
-dotnet build
-dotnet publish
 dotnet pack
 
-latest_package=$(ls ./bin/Release/ChatAIze.GenerativeCS.*.*.*.nupkg | sort -V | tail -n 1)
+nupkg=$(ls ./bin/Release/ChatAIze.GenerativeCS.*.nupkg | grep -v '\.snupkg$' | sort -V | tail -n1)
+snupkg="${nupkg%.nupkg}.snupkg"
 
-dotnet nuget push "$latest_package" --api-key "$CHATAIZE_NUGET_API_KEY" --source "https://api.nuget.org/v3/index.json"
-dotnet nuget push "$latest_package" --api-key "$GITHUB_PAT" --source "https://nuget.pkg.github.com/chataize/index.json"
+dotnet nuget push "$nupkg" --api-key "$CHATAIZE_NUGET_API_KEY" --source "https://api.nuget.org/v3/index.json" --skip-duplicate
+dotnet nuget push "$snupkg" --api-key "$CHATAIZE_NUGET_API_KEY" --source "https://api.nuget.org/v3/index.json" --skip-duplicate
+dotnet nuget push "$nupkg" --api-key "$GITHUB_PAT" --source "https://nuget.pkg.github.com/chataize/index.json" --skip-duplicate
+dotnet nuget push "$snupkg" --api-key "$GITHUB_PAT" --source "https://nuget.pkg.github.com/chataize/index.json" --skip-duplicate
